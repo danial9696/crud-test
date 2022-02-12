367c946873eb2806fffca31b49a7c3f9e93e12c0
d.karshenas <dan.karshenas@gmail.com>
2022-02-12 10:32:45 +0330

    Router & sass packages installed again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "../src/sass/reset.scss";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return <div className="App"></div>;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./router/AppRouter";
 
-ReactDOM.render(
-  <AppRouter>
-    <App />
-  </AppRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
